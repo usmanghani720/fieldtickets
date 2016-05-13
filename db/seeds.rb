e4161debcfc885ae2f1c1865c4bd336372af485a
Oliver Anson <git@fluffyx.com>
2016-05-13 15:59:32 -0400

    clean up seeds again

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
 Customer.create!([
   {name: 'Acme Construction'},
-  {name: 'Florida DOT'}
+  {name: 'Florida DOT'},
 ])
 
 Employee.create!([
   {name: 'John Doe'},
   {name: 'Mark Twain'},
-  {name: 'Vince Smith'}
+  {name: 'Vince Smith'},
 ])
 
 Equipment.create!([
   {internal_number: '007', vehicle_type: 'Truck'},
-  {internal_number: '032', vehicle_type: 'Mill'}
+  {internal_number: '032', vehicle_type: 'Mill'},
 ])
 
 Job.create!([
-  {internal_number: '12345', customer_id: 1}
+  {internal_number: '12345', customer_id: 1},
 ])
 
 FieldTicket.create!([
-  {job_id: 1}
+  {job_id: 1},
 ])
