b46269a15895ad0ce6d8aa8fd1c503566d3a3ee3
Oliver Anson <git@fluffyx.com>
2016-05-14 11:45:25 -0400

    make DisplayName concern smarter

--- app/models/concerns/display_name.rb
+++ app/models/concerns/display_name.rb
@@ -8,4 +8,16 @@ module DisplayName
   def cache_display_name
     self[:display_name] = to_s
   end
+  
+  def display_name
+    # simple logic doesn't work if to_s relies on another model
+    
+    cache_display_name if changed?
+      
+    self[:display_name]
+  end
+  
+  def display_name=(new_value)
+    raise 'display_name is read only!'
+  end
 end
